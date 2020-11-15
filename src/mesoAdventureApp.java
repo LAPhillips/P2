59202be1997b4686c952dfa12e2febdf223d5e57
LAPhillips <phil5544@stthomas.edu>
2020-11-15 16:51:10 -0600

    First Commit - setting up git

---
+++ src/mesoAdventureApp.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author erono
+ *
+ */
+public class mesoAdventureApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+	}
+
+}
